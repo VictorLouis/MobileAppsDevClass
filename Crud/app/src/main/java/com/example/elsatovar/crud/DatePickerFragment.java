c28906711c7796d828ef6f37d62d987b9205da18
Victor Luis <vlangulot@unal.edu.co>
2018-11-15 05:04:32 -0500

    Reto CRUD

---
+++ Crud/app/src/main/java/com/example/elsatovar/crud/DatePickerFragment.java
@@ -0,0 +1,59 @@
+package com.example.elsatovar.crud;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.lang.reflect.AccessibleObject;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+public class DatePickerFragment extends DialogFragment {
+
+    private DatePicker datePicker;
+    private static final int REQUEST_DATE = 1;
+
+    public static final String EXTRA_DATE =
+            "com.example.date";
+
+    public interface DateDialogListener {
+        void onFinishDialog(Date date);
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.activity_add_update_employee,null);
+
+        datePicker = (DatePicker) v.findViewById(R.id.edit_text_hire_date);
+
+
+        return new android.support.v7.app.AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int year = datePicker.getYear();
+                                int mon = datePicker.getMonth();
+                                int day = datePicker.getDayOfMonth();
+                                Date date = new GregorianCalendar(year,mon,day).getTime();
+                                DateDialogListener activity = (DateDialogListener) getActivity();
+                                activity.onFinishDialog(date);
+                                dismiss();
+                            }
+                        })
+                .create();
+    }
+
+
+}
